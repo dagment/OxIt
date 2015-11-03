264424aa770e1fa16fd6b52443cd02da832241c5
dagment <dagment@ukr.net>
2015-11-03 18:31:36 +0200

    Modal edit

--- L9/template/header.php
+++ L9/template/header.php
@@ -24,37 +24,7 @@
 				echo Lib_Menu::getInstance()->getMenu();
 				?>
 			</div>	
-			<div id="modal" class="popupContainer" style="display:none;">
-				<header class="popupHeader">
-						<span class="header_title">Login</span>
-						<span class="modal_close"><i class="fa fa-times"></i></span>
-				</header>
-
-				<section class="popupBody">
-						
-						<!-- Username & Password Login form -->
-						<div class="user_login">
-								<form action="/enter" method="POST">
-										<label>Username</label>
-										<input type="text" name="login" value="<?=$login?>"/>
-										<br />
-
-										<label>Password</label>
-										<input type="password" name="pass" value="<?=$pass?>"/>
-										<br />
-
-
-										<div class="action_btns">
-												<div class="one_half"><a href="" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a></div>
-												<div class="one_half last"><input type="submit" class="btn btn_red" value="Вхід"></div>
-										</div>
-								</form>
-
-								
-						</div>
-
-				</section>
-		</div>
+<?require('modal.php');?>
 		</div>
 		<div class="hrr">
 		<hr size="5" color="#000000">
